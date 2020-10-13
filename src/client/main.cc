0a64b5862cb0d250b8c900a5c02a2219f53fdf83
Daniel Kyrillov <jedi5218@gmail.com>
2020-10-13 11:48:00 +0300

    implement basic client for the echo-server

--- src/client/main.cc
+++ src/client/main.cc
@@ -1,19 +1,94 @@
+#include <arpa/inet.h>
+#include <cctype>
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <getopt.h>
 #include <iostream>
-#include "argsparser.h"
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "common/argsparser.h"
 
 int main(int argc, char *argv[])
 {
-    Argument port('p', "port", true);
+    Argument port_param('p', "port", true);
+    Argument echo('s', "string", true);
     Argument ip_addr('i', "ip-addr", true);
-    Argument cow('c', "cow", false);
 
-    if (!Argument::parse_arguments({port, ip_addr, cow}, argc, argv, "The server application."))
+    short port = 8080;
+    if (!Argument::parse_arguments({port_param, ip_addr, echo}, argc, argv, "The client application."))
         return 0;
-
-    if (port.is_set())
-        std::cout << "listening on the port" << port.value() << std::endl;
+    if (port_param.is_set())
+        try
+        {
+            port = std::stoi(port_param.value());
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "Port parameter must be a number" << std::endl;
+            return 0;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr
+                << "Error encountered while reading port number: "
+                << e.what() << std::endl;
+            return 1;
+        }
     else
-        std::cout << "port was not provided, defaulting to 8080" << std::endl;
+        std::cout << "port was not provided, defaulting to " << port << std::endl;
 
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        printf("Socket creation failed. %s\n", strerror(errno));
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(ip_addr.value().c_str());
+    servaddr.sin_port = htons(port);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, reinterpret_cast<sockaddr *>(&servaddr), sizeof(servaddr)) != 0)
+    {
+        printf("connection with the server failed. %s\n", strerror(errno));
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+    char buff[256];
+    bzero(buff, sizeof(buff));
+    memcpy(buff, echo.value().c_str(), sizeof(buff) - 1);
+    int retval = write(sockfd, buff, sizeof(buff));
+    if (retval <= 0)
+    {
+        if (retval < 0)
+            printf("Unexpected error while sending. id #%i: %s\n", errno, strerror(errno));
+        printf("Connection with server was closed before the query was sent.\n");
+        close(sockfd);
+        return 0;
+    }
+    bzero(buff, sizeof(buff));
+    retval = read(sockfd, buff, sizeof(buff));
+    if (retval <= 0)
+    {
+        if (retval < 0)
+            printf("Unexpected error while recieving. id #%i: %s\n", errno, strerror(errno));
+        printf("Connection with server was closed before the response was recieved.\n");
+        close(sockfd);
+        return 0;
+    }
+    printf("Processed string: %s\n", buff);
+    close(sockfd);
     return 0;
 }
